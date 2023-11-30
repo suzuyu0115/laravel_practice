d611454d2b83177e3a3a6db035d0d1068d6ec6a7
suzuyu0115 <szkyut0115@gmail.com>
2023-12-01 01:34:55 +0900

    Add: コンタクトフォームコントローラの作成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('tests/test', [TestController::class, 'index']);
+
+// Route::resource('contacts', ContactFormController::class);
 
 Route::get('/', function () {
     return view('welcome');
