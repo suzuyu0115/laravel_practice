072985c925cf6a3ff8075b4915bdf738e26a7db0
suzuyu0115 <szkyut0115@gmail.com>
2023-11-30 16:00:49 +0900

    Add: クエリビルダ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
+        // Eloquent(エロクアント)
         $values = Test::all();
 
-        // dd($values);
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'aaa')->get();
+
+        // クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'にょっす🐮✋')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
 
         return view('tests.test', compact('values'));
     }
