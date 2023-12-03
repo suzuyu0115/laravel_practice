50b55a5dd0128ef4a4006e0d5d0b921347711260
suzuyu0115 <szkyut0115@gmail.com>
2023-12-03 17:26:54 +0900

    Fix: ファットコントローラ対策

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ContactForm;
+use App\Services\CheckFormService;
 use Illuminate\Http\Request;
 
 class ContactFormController extends Controller
@@ -63,18 +64,9 @@ class ContactFormController extends Controller
     {
         $contact = ContactForm::find($id);
 
-        if ($contact->gender === 0) {
-            $gender = '男性';
-        } else {
-            $gender = '女性';
-        }
+        $gender = CheckFormService::checkGender($contact);
 
-        if ($contact->age === 1) { $age = '〜19歳'; }
-        if ($contact->age === 2) { $age = '20〜29歳'; }
-        if ($contact->age === 3) { $age = '30〜39歳'; }
-        if ($contact->age === 4) { $age = '40〜49歳'; }
-        if ($contact->age === 5) { $age = '50〜59歳'; }
-        if ($contact->age === 6) { $age = '60歳〜'; }
+        $age = CheckFormService::checkAge($contact);
 
         return view('contacts.show', compact('contact', 'gender', 'age'));
     }
